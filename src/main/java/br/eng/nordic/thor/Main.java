a72250aee9eccc98653f2279262d8e73c9c046d7
unknown <davipeter7@gmail.com>
2025-08-25 17:04:04 -0300

    inicio do projeto

---
+++ src/main/java/br/eng/nordic/thor/Main.java
@@ -0,0 +1,7 @@
+package br.eng.nordic.thor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/br/eng/nordic/thor/Ser.java
@@ -0,0 +1,5 @@
+package br.eng.nordic.thor;
+
+public class Ser {
+
+}
